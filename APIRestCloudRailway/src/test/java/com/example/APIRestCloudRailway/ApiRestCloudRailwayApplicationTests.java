d11ef05d11e075fa23e73c761017828d646e3888
Dyogo199 <dyogo_111@hotmail.com>
2024-10-24 10:19:25 -0300

    Inicio do projeto ApiRest com Railway

---
+++ APIRestCloudRailway/src/test/java/com/example/APIRestCloudRailway/ApiRestCloudRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIRestCloudRailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCloudRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
